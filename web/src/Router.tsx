e9596ef9f773cc7e95397ac3ee2086edb478e5f3
Sam Dunster <me@sdunster.com>
2026-06-07 06:16:40 +1000

    Redirect /scan to /kiosk so new bookmarks/homepages use the /kiosk url

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 import { lazyWithReload } from "./lib/lazyWithReload";
 import LoadingIndicator from "./components/LoadingIndicator";
@@ -30,8 +30,8 @@ export default function Router() {
           <Route path="/admin/*" element={<AdminApp />} />
 
           {/* Kiosk routes - auth required at /kiosk */}
-          <Route path="/scan" element={<KioskMain />} />
-          <Route path="/scan/status-demo" element={<StatusDemo />} />
+          <Route path="/scan" element={<Navigate to="/kiosk" replace />} />
+          <Route path="/kiosk/status-demo" element={<StatusDemo />} />
           <Route path="/kiosk" element={<KioskMain />} />
           <Route path="/kiosk/:profile" element={<KioskMain />} />
 
